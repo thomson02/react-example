efd28399bd4b1644d03009db988561382322c580
EMEA\i670426 <andrew.r.thomson@jpmorgan.com>
2019-08-09 10:08:49 +0100

    add build dir

---
+++ build/precache-manifest.6175c4fe064702734f6a59b685a12daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fac9b0049c50c66a807872262b05f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4848031669e2fb4a6a",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "7e4848031669e2fb4a6a",
+    "url": "/static/js/main.838a2cfb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
